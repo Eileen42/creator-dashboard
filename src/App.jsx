1da27556a4fb7e0f87dbe72fbab79cd06ad0722a
Eileen42 <77676327+Eileen42@users.noreply.github.com>
2025-11-29 15:48:08 +0900

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import { Youtube, Instagram, Music2, TrendingUp, DollarSign, Video, Plus, ArrowL
 // ============================================
 const CONFIG = {
   GOOGLE_CLIENT_ID: '590021584308-rfnvvdjmntukh5roq0dlp6hibf470njs.apps.googleusercontent.com',
-  APPS_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbyFXqPYbKSe4MqMBlwYOmopmvIf67Iimic8LyzlaFYaf3vjcvXrlkOMV_AI32NkZzReLA/exec'
+  APPS_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbwHI6f0hVspBA8vftjOvNhnrYBXJA1fJIF9R3SdvACN7v82xEV-YwyUGhEgfktMbxOSeg/exec'
 };
 
 // ============================================
